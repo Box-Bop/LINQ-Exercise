867b683a7c8e7ee75dd1982c79092e52269c518f
Box-Bop <Box-Bop@github.com>
2018-02-23 18:27:30 +0200

    All done

--- LINQExrcse/Program.cs
+++ LINQExrcse/Program.cs
@@ -18,19 +18,19 @@ namespace LINQExrcse
             StreamReader sr = new StreamReader(textFilePath);
 
             int counter = 0;
-            string delim = " ,.";
+            string delim = " .'";
             string[] fields = null;
             string line = null;
 
             while (!sr.EndOfStream)
             {
-                line = sr.ReadLine();
+                line = sr.ReadToEnd();
                 line.Trim();
                 fields = line.Split(delim.ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
                 counter += fields.Length;
             }
-            Console.WriteLine("The amount of words in the text file (with \" ,.\" being the delimiters/splitters): {0} ", counter);
-            Console.WriteLine("Press enter for exercise 2.");
+            Console.WriteLine("The amount of words in the text file (with \"" + delim + "\" being the delimiters/splitters): {0} ", counter);
+            Console.WriteLine("\nPress enter for exercise 2.");
             Console.ReadLine();
 
             #endregion Number 1.
@@ -50,17 +50,16 @@ namespace LINQExrcse
             {
                 if (item == " ")
                 { }
-                if (!uniqueLetters.Contains(Convert.ToString(item.ToLower())))
+                if (!uniqueLetters.Contains(Convert.ToString(item)))
                 {
                     uniqueLetters.Add(Convert.ToString(item));
+                    Console.Write(item);
                 }
                 else
                 { }
             }
-
-            uniqueLetters.ForEach(Console.Write);
             Console.WriteLine();
-            Console.WriteLine("\nThe list above shows all of the unique characters in the \"tekst.txt\" file.\n\nPress enter for exercise 3");
+            Console.WriteLine("\nThe list above shows all of the unique characters (upper and lower) in the \"tekst.txt\" file.\n\nPress enter for exercise 3");
             Console.ReadLine();
 
             #endregion Number 2.
@@ -69,26 +68,38 @@ namespace LINQExrcse
 
             Console.Clear();
 
-            //var fileLetters = new List<string>();
-            //foreach (var item in File.ReadAllText(textFilePath))
-            //{
-            //    fileLetters.Add(Convert.ToString(item));
-            //}
+            var letterFrequency = File.ReadAllText(textFilePath)
+                    .Where(c => Char.IsLetter(c))
+                    .GroupBy(c => c)
+                    .ToDictionary(g => g.Key, g => g.Count());
 
-            //var uniqueLetters = new List<string>();
-            //foreach (var item in fileLetters)
-            //{
-            //    if (uniqueLetters.Contains(item))
-            //    { }
-            //    if (item == " ")
-            //    { }
-            //    else
-            //    {
-            //        uniqueLetters.Add(item);
-            //    }
-            //}
+            foreach (KeyValuePair<char, int> kvp in letterFrequency)
+            {
+                Console.WriteLine("Letter: {0}  Frequency {1}", kvp.Key, kvp.Value);
+            }
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("\nThe above list displays the text file character frequencies (Both upper and lower case)\n\nPress enter for exercise 4");
+            Console.ReadLine();
 
             #endregion Number 3.
+
+            #region Number 4.
+
+            var uniqueWords = new List<string>();
+
+            foreach (var item in fields)
+            {
+                if (!uniqueWords.Contains(item.ToLower()))
+                {
+                    uniqueWords.Add(item);
+                    Console.Write(item + " ");
+                }
+                else { }
+            }
+            Console.WriteLine("\n");
+            Console.WriteLine("\nThe above text is all of the \"tekst.txt\" text, but with no word repeating twice.\n\nMOST OF THE QUESTION MARKS ARE ACTUALLY UPPER COMMAS\nRESULTS NOT PERFECT DUE TO THAT ^");
+
+            #endregion Number 4.
 
             Console.ReadLine();
         }
